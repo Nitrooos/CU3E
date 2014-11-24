013bd9cad79e9f913b0f8dae6d00ff1b2f511dd2
HFScofield <HFScofield@gmail.com>
2014-11-24 19:27:39 +0100

    Improved calibration proces
    Better calibration and a lot of azer staf

--- Capturing/Capturing/main.cpp
+++ Capturing/Capturing/main.cpp
@@ -1,176 +1,39 @@
 #include "capture.h"
+/*
+*	capture(arg)
+*	arg:
+*		1 - DETECTING VERTICES						<requires 3>
+*		2 - DETECT VERTICES ONCE					<requires 3>
+*		3 - camera stuff initialization				<VIDEO INIT>
+*		4 - calibration								<requires 3>
+*		5 - camera stuff finalization				<VIDEO FINA>
+*		6 - processing images using filters			<>
+*		7 - calibration on images					<>
+*		8 - take pictures to use in 3 and 4			<requires 3>
+*/
+
 
 int main(int argc, char** argv){
-	//return calibrate();
-	//return imgProcess();
-
-	VideoCapture cap(0);	//capture the video from Cam 0
-	if (!cap.isOpened()){
-		cout << "ERROR: Cannot open Cam 0!" << endl;
-		return -1;
-	}
-	return capturing(&cap);
-}
-
-//Video Capturing
-/*
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <omp.h>
-#include "opencv2/highgui/highgui.hpp"
-
-#define IMG_DIR "output/"
-
-using namespace cv;
-using namespace std;
-
-int main(int argc, char* argv[]){
-	unsigned i=5;
-	bool stop = false;
-	double dWidth;
-	double dHeight;
-	bool bSuccess;
-	string name;
-	Mat frame;
-	int key;
-
-	VideoCapture cam(0);		// open the video
-	if (!cam.isOpened()){		// if not success, exit program
-		cout << "ERROR: Cannot open Cam 0!" << endl;
-		return -1;
-	}
-	dWidth = cam.get(CV_CAP_PROP_FRAME_WIDTH);		//get the width of frames
-	dHeight = cam.get(CV_CAP_PROP_FRAME_HEIGHT);	//get the height of frames
-	cout << "Frame size of Cam 0: "  << dWidth << " x " << dHeight << endl;
-	namedWindow("Cam 0", CV_WINDOW_AUTOSIZE);		//create a window
-
-	i = 0;
-	//omp_set_num_threads(2);
-	while (!stop){
-		//#pragma omp parallel for schedule(static, CAM_NUM)
-		bSuccess = cam.read(frame);	//read a new frame
-		if (bSuccess){
-			imshow("Cam 0", frame);	//show the frame
-
-			//name = "image_" + to_string(i) +".jpg";
-			//imwrite(IMG_DIR + name, frame);
-			//cout << name << endl;
-
-			key = waitKey(20);
-			if (key == 27){			//wait for ESC key press for 20ms
-				cout << endl << "ESC key is pressed by user." << endl;
-				stop = true;
-			}
-			
-			if (key == 99){
-				i++;
-				name = "image_" + to_string(i) +".jpg";
-				imwrite(IMG_DIR + name, frame);
-				cout << name << endl;
+	int result = 0;
+	for (int i = 1; i < argc; i++){
+		if (atoi(argv[i]) == 2){
+			Vec3d vec;
+			cout << "You are using Single Vertices Detection in infinite loop. Press ESC to stop." << endl;
+			while (1){
+				capture(2, &vec);
+				cout << "\r" << vec;
+				if (waitKey(20) == 27){		//ESC - exit
+					cout << "ESC key pressed by the user." << endl;
+					break;
+				}
 			}
 		}
 		else{
-			cout << "ERROR: Cannot read a frame from Cam 0." << endl;
+			result += capture(atoi(argv[i]));	//"capture()" decides what function to execute
 		}
 	}
-	cam.~VideoCapture();
-	destroyWindow("Cam 0");
-
-	cout << endl << "Cam 0 stopped." << endl << endl << "Press ENTER to end...";
-	getchar();
-	return 0;
+	return result;
 }
-*/
-
-//Corner detection
-/*
-#include "opencv2/highgui/highgui.hpp"
-#include "opencv2/imgproc/imgproc.hpp"
-#include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
-
-using namespace cv;
-using namespace std;
-
-/// Global variables
-Mat src, src_gray;
-int thresh = 200;
-int max_thresh = 255;
-
-char* source_window = "Source image";
-char* corners_window = "Corners detected";
-
-/// Function header
-void cornerHarris_demo(int, void*);
-
-/// @function main
-int main(int argc, char** argv){
-	bool stop = false;
-	/// Load source image and convert it to gray
-	VideoCapture cam(0);
-	if (!cam.isOpened()){
-		cout << "ERROR: Cannot open Cam 0!" << endl;
-		return -1;
-	}
-	int i = 0;
-	while (!stop){
-		i++;
-		if (!cam.read(src)){
-			cout << "ERROR: Cannot get frame from Cam 0!" << endl;
-			return -1;
-		}
-		cout << i << "\ttake_pic";
-		cvtColor(src, src_gray, CV_BGR2GRAY);
-
-		/// Create a window and a trackbar
-		namedWindow(source_window, CV_WINDOW_AUTOSIZE);
-		createTrackbar("Threshold: ", source_window, &thresh, max_thresh, cornerHarris_demo);
-		imshow(source_window, src);
-		cout << " Harris_begin";
-		cornerHarris_demo(0, 0);
-		cout << " Harris_end" << endl;
-
-		if (waitKey(30) == 27){			//wait for ESC key press for 20ms
-			cout << endl << "ESC key is pressed by user." << endl;
-			stop = true;
-		}
-	}
-	return(0);
-}
-
-/// @function cornerHarris_demo
-void cornerHarris_demo(int, void*){
-	Mat dst, dst_norm, dst_norm_scaled;
-	dst = Mat::zeros(src.size(), CV_32FC1);
-
-	/// Detector parameters
-	int blockSize = 2;
-	int apertureSize = 3;
-	double k = 0.03;
-
-	/// Detecting corners
-	cornerHarris(src_gray, dst, blockSize, apertureSize, k, BORDER_DEFAULT);
-
-	/// Normalizing
-	normalize(dst, dst_norm, 0, 255, NORM_MINMAX, CV_32FC1, Mat());
-	convertScaleAbs(dst_norm, dst_norm_scaled);
-
-	/// Drawing a circle around corners
-	for (int j = 0; j < dst_norm.rows; j++){
-		for (int i = 0; i < dst_norm.cols; i++){
-			if ((int)dst_norm.at<float>(j, i) > thresh){
-				circle(dst_norm_scaled, Point(i, j), 5, Scalar(0), 2, 8, 0);
-			}
-		}
-	}
-
-	/// Showing the result
-	namedWindow(corners_window, CV_WINDOW_AUTOSIZE);
-	imshow(corners_window, dst_norm_scaled);
-}
-*/
 
 //Color writing
 /*
@@ -191,10 +54,10 @@ int main(int argc, char** argv){
 
 	namedWindow("Control", CV_WINDOW_AUTOSIZE); //create a window called "Control"
 
-	int iLowH = 170;
-	int iHighH = 179;
+	int iLowH = 0;
+	int iHighH = 14;
 
-	int iLowS = 150;
+	int iLowS = 1;
 	int iHighS = 255;
 
 	int iLowV = 60;
@@ -279,5 +142,4 @@ int main(int argc, char** argv){
 		}
 	}
 	return 0;
-}
-*/
+}*/
